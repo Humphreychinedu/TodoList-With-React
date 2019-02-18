dd33669d6ffc10f6c12732ff0d47eb6c2c83fa9a
Humphreychinedu <mefendja.chinedu@gmail.com>
2019-02-18 07:49:28 +0100

    A complete react project with an API call to jason placeholder

---
+++ src/components/Todos.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
+
+class Todos extends Component {
+  // markComplete = (e) => {
+  //   console.log("hello")
+  // }
+
+  render() {
+    return (
+        this.props.todos.map((todo) =>
+           <TodoItem key={todo.id} todo={todo} markComplete= {this.props.markComplete} deleteItem = {this.props.deleteItem}/>      
+        )
+    );
+  }
+}
+
+//PropTypes
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  deleteItem: PropTypes.func.isRequired
+}
+export default Todos;
